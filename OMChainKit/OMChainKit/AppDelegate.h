be37f24ab4b7ef1739c4d7f316e530ab94bc13c1
Zane Helton <zane99@me.com>
2015-03-15 00:04:53 -0400

    Initial Commit

---
+++ OMChainKit/OMChainKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OMChainKit
+//
+//  Created by Zane Helton on 3/14/15.
+//  Copyright (c) 2015 ZaneHelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OMChainKit/OMChainKit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OMChainKit
+//
+//  Created by Zane Helton on 3/14/15.
+//  Copyright (c) 2015 ZaneHelton. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
